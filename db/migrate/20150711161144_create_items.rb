35ad3d99011cd434dad01baeb9110ac8ece66fd6
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-11 10:15:49 -0600

    added item model.

---
+++ db/migrate/20150711161144_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.integer :quantity
+      t.string :brand
+      t.text :instructions
+      t.timestamps null: false
+    end
+  end
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711155855) do
+ActiveRecord::Schema.define(version: 20150711161144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20150711155855) do
     t.string   "type"
     t.integer  "user_id"
     t.integer  "time_frame"
+    t.text     "instructions"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "quantity"
+    t.string   "brand"
     t.text     "instructions"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false

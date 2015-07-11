b1b355f2803622a291921fdee11f21f16681a210
Carter Graves <carterg31@gmail.com>
2015-07-11 12:30:10 -0600

    sms integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,19 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  def send_sms(recipient, body)
+    @client = setup_twilio_client
+    @client.account.messages.create(
+      from: ENV['twilio_number'],
+      to: recipient.phone,
+      body: body
+    )
+  end
+
+  private
+
+  def setup_twilio_client
+    Twilio::REST::Client.new ENV['account_sid'], ENV['auth_token']  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+
+  post "baskets/:basket_id/send_sms" => "users#send_sms", as: :send_sms
 end

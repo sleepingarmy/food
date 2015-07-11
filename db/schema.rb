f3dcff75380e6a72d67b178ac6004191eee5d3f7
Doyi Lee <doyilee@hotmail.com>
2015-07-11 14:59:05 -0600

    removed unnecessary url path for all forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711201538) do
+ActiveRecord::Schema.define(version: 20150711205042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150711201538) do
     t.text     "instructions"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "basket_id"
   end
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|

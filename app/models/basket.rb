73574318e3e7be8b5ed76222d08b0cb6ad532b06
Rosemary Thomas <rsm.thomas@gmail.com>
2015-07-11 12:21:45 -0600

    added class methods to baskets model

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,2 +1,22 @@
 class Basket < ActiveRecord::Base
+  belongs_to :user
+  self.inheritance_column = :type
+
+  def self.types 
+    %w(Grocery Restaurant General)
+  end
+
+  #scopes
+  def self.grocery
+    where(type: 'Grocery')
+  end
+
+  def self.restaurant
+    where(type: 'Restaurant')
+  end
+
+  def self.general
+    where(type: 'General')
+  end
+
 end
--- app/models/grocery.rb
+++ app/models/grocery.rb
@@ -1,2 +1,3 @@
 class Grocery < Basket
+  
 end

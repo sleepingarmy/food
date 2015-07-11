3a236115c1f8ef50ba70f7ad0e16ea93a0b60bb2
Doyi Lee <doyilee@hotmail.com>
2015-07-11 11:08:37 -0600

    updated users controller

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,3 +1,23 @@
 class AddressController < ApplicationController
-	
+	def index
+		@address = @user.index
+		
+	end
+
+	def new
+
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+
+	end
+
+	def destroy
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,38 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  # def create
+  #   @user = User.create(user_params)
+  # end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      flash[:notice]="Person was updated successfully!"
+      redirect_to(users_path)
+    else
+      flash[:error]="Person was not updated."
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to :back, :alert => "Access denied."
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :email)
   end
 
 end

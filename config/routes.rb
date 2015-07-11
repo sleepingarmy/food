fc877d645c3cbf7bfc1464cced5cd6c041283d37
Doyi Lee <doyilee@hotmail.com>
2015-07-11 14:22:59 -0600

    add address routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   post "baskets/:basket_id/send_sms" => "users#send_sms", as: :send_sms
 
+  resrouces :addresses
+  
   resources :baskets do
     resources :items do
     end
